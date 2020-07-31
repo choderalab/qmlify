1dbcd869d831c8502df3316fb582d027e63662ef
dominicrufa <dominic.rufa@choderalab.org>
2020-07-31 16:51:28 -0400

    debug analysis for data aggregation

--- qmlify/analysis.py
+++ qmlify/analysis.py
@@ -7,11 +7,11 @@ import numpy as np
 import os
 
 DEFAULT_WORK_TEMPLATE = 'lig{i}to{j}.{phase}.{state}.{direction}.idx_{idx}.{annealing_steps}_steps.works.npz'
-DEFAULT_WORK_QUERY_TEMPLATE = '.'.join(DEFAULT_WORK_TEMPLATE.split('.')[:4]) + '.*.' + '.'join(DEFAULT_WORK_TEMPLATE.split('.')[5:])
+#DEFAULT_WORK_QUERY_TEMPLATE = '.'.join(DEFAULT_WORK_TEMPLATE.split('.')[:4]) + '.*.' + '.'join(DEFAULT_WORK_TEMPLATE.split('.')[5:])
 
 
 
-def aggregate_per_pair_works(ligand_indices, annealing_steps, directions = ['forward', 'backward'], phases = ['complex', 'solvent'], states = ['old', 'new'], parent_dir = os.getcwd()):
+def aggregate_per_pair_works(ligand_indices, annealing_steps, directions = ['forward', 'backward'], states = ['old', 'new'], parent_dir = os.getcwd()):
     """
     generate a dictionary of the final aggregated works of the trajectories with the specifications;
     """
@@ -21,19 +21,19 @@ def aggregate_per_pair_works(ligand_indices, annealing_steps, directions = ['for
     pair_works_dict = {
                         (i,j): {state: {phase: {direction: {} for direction in directions
                                                     }
-                                           for phase in phases }
+                                           for phase in list(annealing_steps.keys()) }
                                    for state in states }
                         for i,j in ligand_indices}
 
     for i,j in tqdm.tqdm(ligand_indices):
         for state in states:
-            for phase in phases:
+            for phase in list(annealing_steps.keys()):
                 for direction in directions:
-                    file_dict = work_file_extractor(i, j, phase, state, annealing_steps, parent_dir) 
+                    file_dict = work_file_extractor(i, j, phase, state, direction, annealing_steps[phase], parent_dir) 
                     for idx, filename in file_dict.items():
                         try:
                             works = np.load(filename)['works']
-                            assert len(works) == annealing_steps + 1
+                            assert len(works) == annealing_steps[phase] + 1
                             pair_works_dict[(i,j)][state][phase][direction][idx] = works[-1]
                         except Exception as e:
                             print(f"aggregate_per_pair_works query error: {e}")
@@ -54,34 +54,16 @@ def generate_duplication_histograms(pair_works_dict, write_dir, fig_width=8.5, f
     
     #fig = plt.figure(figsize=(fig_width, fig_height))
     
-    
-    
 
-def compute_free_energy_corrections(ligand_indices, annealing_steps, compute_EXP = False, generate_plots=True, write_dir = os.getcwd()):
-    """
-    generate a dictionary of BAR and EXP free energy corrections.
-    """
-    #run the aggregate_per_pair_works to get a dict of forward/backward works per pair, phase, etc
-    from qmlify.analysis import aggregate_per_pair_works
-    pair_works_dict = aggregate_per_pair_works(ligand_indices, annealing_steps, states, parent_dir = parent_dir)
-    
-    #save a matplotlib pdf plot of these works
-    if generate_plots
-        
-
-    #aggregate the works into single dict for each ligand for each phase
-    
-    #save a matplotlib pdf of these works
-    
-    #write a dict and return it
-
-def work_file_extractor(i, j, phase, state, annealing_steps, parent_dir):
+def work_file_extractor(i, j, phase, state, direction, annealing_steps, parent_dir):
     """
     pull the indices of all existing work files
     """
     import glob
-    work_query_template = '.'.join(DEFAULT_WORK_TEMPLATE.split('.')[:4]) + '.*.' + '.'join(DEFAULT_WORK_TEMPLATE.split('.')[5:])
-    work_filenames = glob.glob(work_query_template)
+    import os
+    work_query_template = os.path.join(parent_dir, '.'.join(DEFAULT_WORK_TEMPLATE.split('.')[:4]) + '.*.' + '.'.join(DEFAULT_WORK_TEMPLATE.split('.')[5:]))
+    work_query_filename = work_query_template.format(i=i, j=j, phase=phase, state=state, direction=direction, annealing_steps=annealing_steps)
+    work_filenames = glob.glob(work_query_filename)
     index_extractions = {int(filename.split('.')[4][4:]): os.path.join(parent_dir, filename) for filename in work_filenames}
     return index_extractions
 
